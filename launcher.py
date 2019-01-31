2b0f7915f9e5d5d695bb9cee3856e7f8f0c8bbce
danhitchcock <daniel.s.hitchcock@gmail.com>
2019-01-31 18:58:58 -0500

    -added a launcher to restart the process if it dies

---
+++ launcher.py
@@ -0,0 +1,9 @@
+import subprocess
+filename = 'nano_tipper_z.py'
+while True:
+    p = subprocess.Popen('python ' +filename, shell=True).wait()
+    if p != 0:
+        continue
+    else:
+        break
+
