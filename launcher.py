72c2e6f4b224eb4e22024c6346bdd4430583fa7b
danhitchcock <daniel.s.hitchcock@gmail.com>
2019-02-03 11:44:43 -0500

    added a delay to restart the bot

--- launcher.py
+++ launcher.py
@@ -1,8 +1,10 @@
 import subprocess
+from time import sleep
 filename = 'nano_tipper_z.py'
 while True:
-    p = subprocess.Popen('python ' +filename, shell=True).wait()
+    p = subprocess.Popen('python ' + filename, shell=True).wait()
     if p != 0:
+        sleep(10)
         continue
     else:
         break
