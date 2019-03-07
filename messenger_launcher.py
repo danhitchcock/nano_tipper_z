59f0df284fafa4501b3e533e6d07d781f41b2066
danhitchcock <daniel.s.hitchcock@gmail.com>
2019-03-07 13:28:45 -0500

    -moved tipper username to separate file for config

--- messenger_launcher.py
+++ messenger_launcher.py
@@ -5,6 +5,7 @@ with open('python_version') as f:
     python_version = f.read()
 while True:
     p = subprocess.Popen(python_version + ' ' + filename, shell=True).wait()
+    print('something went wrong.')
     if p != 0:
         sleep(10)
         continue
