b5199c022502f464199bdd5988ca6f2c460731db
danhitchcock <daniel.s.hitchcock@gmail.com>
2019-03-07 13:36:32 -0500

    -moved tipper username to separate file for config

--- launcher.py
+++ launcher.py
@@ -2,7 +2,7 @@ import subprocess
 from time import sleep
 filename = 'nano_tipper_z.py'
 with open('python_version') as f:
-    python_version = f.read()
+    python_version = f.read().replace('\n', '')
 while True:
     p = subprocess.Popen(python_version + ' ' + filename, shell=False).wait()
     if p != 0:
--- messenger_launcher.py
+++ messenger_launcher.py
@@ -2,7 +2,7 @@ import subprocess
 from time import sleep
 filename = 'messenger.py'
 with open('python_version') as f:
-    python_version = f.read()
+    python_version = f.read().replace('\n', '')
 while True:
     p = subprocess.Popen(python_version + ' ' + filename, shell=False).wait()
     print('something went wrong.')
