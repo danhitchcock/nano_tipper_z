cc834b79558d5efe94d637658f3df90edf4e5a41
danhitchcock <daniel.s.hitchcock@gmail.com>
2019-03-07 13:39:09 -0500

    -moved tipper username to separate file for config

--- launcher.py
+++ launcher.py
@@ -4,7 +4,7 @@ filename = 'nano_tipper_z.py'
 with open('python_version') as f:
     python_version = f.read().replace('\n', '')
 while True:
-    p = subprocess.Popen(python_version + ' ' + filename, shell=False).wait()
+    p = subprocess.Popen([python_version, filename], shell=False).wait()
     if p != 0:
         continue
     else:
