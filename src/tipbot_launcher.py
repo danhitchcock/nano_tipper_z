ed6067d4ac4f0978074a2b939b02bc8f45a8f04b
danhitchcock <daniel.s.hitchcock@gmail.com>
2019-12-08 10:31:15 -0500

    - fixed launch command

--- src/tipbot_launcher.py
+++ src/tipbot_launcher.py
@@ -3,7 +3,7 @@ import configparser
 from time import sleep
 from translations import python_command, tipper_options
 
-filename = "nano_tipper_z.py"
+filename = "tipbot.py"
 while True:
     p = subprocess.Popen(python_command + " " + filename, shell=True).wait()
     if p != 0:
