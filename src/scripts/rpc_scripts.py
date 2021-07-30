a2c9b2b051a8f8e67078dbb8915fa1dbcd9457dc
danhitchcock <daniel.s.hitchcock@gmail.com>
2021-07-30 12:46:08 -0400

    Add total funds command

--- src/scripts/rpc_scripts.py
+++ src/scripts/rpc_scripts.py
@@ -29,3 +29,17 @@ def all_pendings(threshold):
     for username, address in zip(usernames, addresses):
         if pendings["blocks"][address]:
             print(username, address, pendings["blocks"][address])
+
+@click.command()
+def total_funds():
+    MYCURSOR.execute("SELECT username, address FROM accounts")
+    myresult = MYCURSOR.fetchall()
+    usernames = [str(result[0]) for result in myresult]
+    addresses = [str(result[1]) for result in myresult]
+    MYDB.commit()
+    balance = 0
+    for username, address in zip(usernames, addresses):
+        new_balance = tipper_rpc.check_balance(address)
+        balance += new_balance[0] / 10**30 + new_balance[1] / 10**30
+    print("Total Nano: ", balance)
+
